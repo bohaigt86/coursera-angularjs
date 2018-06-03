0f284ec685edc53d1adb381eca97a0de7d4fd604
Brian Liu <liubohai922@hotmail.com>
2018-06-04 03:35:55 +0800

    update navbar

--- module4-solution/src/items.controller.js
+++ module4-solution/src/items.controller.js
@@ -7,11 +7,13 @@
   ItemsController.$inject = ['items']
   function ItemsController(items) {
     var itemCtrl = this;
+    // console.log("items.data:");
+    // console.log(items.data);
 
-    // console.log(items);
+    itemCtrl.name = items.data.category.name;
+    console.log(itemCtrl.name);
 
     itemCtrl.items = items.data.menu_items;
-
     console.log(itemCtrl.items);
 
   }
